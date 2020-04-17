6a3a47f74e0cbf34788be06d070e6a3b20eacda5
Shuying <zshuying@connect.hku.hk>
2020-04-17 14:56:47 +0800

    update methods to generate variable list, now about 500 variables

---
+++ mycode/generateMatrix.py
@@ -0,0 +1,9 @@
+from utils import *
+itemDic=createItemPositionDict(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir),'myvariablesList.csv'))
+l1=pd.read_csv(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir),'myvariablesList.csv'),header=None).values.tolist()
+l2=pd.read_csv(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir),'delete.csv'),header=None).values.tolist()
+l=list(set(l1)-set(l2))
+variableList=l
+patientHandler=PatientHandler(variableList)
+patientHandler.gen_data()
+#patientHandler.write_delete()
